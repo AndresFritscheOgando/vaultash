89f595d4b1d4351161ee1f140a54ffc23a96de48
AndresFritscheOgando <andresf00909@gmail.com>
2025-12-07 19:06:16 -0400

    implemented cors rules

--- src/utils/index.rs
+++ src/utils/index.rs
@@ -1,4 +1,5 @@
 use axum::Json;
+use tower_http::cors::{CorsLayer, Any};
 use passwords::PasswordGenerator;
 use serde_json::json;
 
@@ -16,4 +17,26 @@ pub async fn generate_password() -> Json<serde_json::Value> {
 
     let password = pg.generate_one().unwrap();
     Json(json!({ "password": password }))
+}
+
+pub fn cors_layer() -> CorsLayer {
+    let cors = CorsLayer::new()
+        // Allow requests from any origin (not recommended for production)
+        // .allow_origin(Any) 
+
+        // OR, specify allowed origins for better security
+        .allow_origin([
+            "http://localhost:3000".parse().unwrap(),
+            "https://your-frontend-domain.com".parse().unwrap(),
+        ])
+        // Allow specific headers like Authorization and Content-Type
+        .allow_headers(Any)
+        // Allow GET and POST methods
+        .allow_methods([
+            http::Method::GET,
+            http::Method::POST,
+            http::Method::DELETE,
+            http::Method::PUT,
+        ]);
+        cors
 }
