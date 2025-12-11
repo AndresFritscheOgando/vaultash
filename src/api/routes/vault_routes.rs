1e537df97c57561c9088f836230f8fba2640a2ee
AndresFritscheOgando <andresf00909@gmail.com>
2025-12-11 00:29:14 -0400

    root path hello world

--- src/api/routes/vault_routes.rs
+++ src/api/routes/vault_routes.rs
@@ -1,21 +1,16 @@
-use axum::{Router, routing::{get}};
-use crate::generate_password;
 use crate::api::handlers::vault_handler::{
-    delete_async,
-    get_by_id_async,
-    create_async,
-    update_async,
-    get_all_async,
+    create_async, delete_async, get_all_async, get_by_id_async, update_async,
 };
-
+use crate::generate_password;
+use axum::{Router, routing::get};
 
 pub fn routes() -> Router {
     Router::new()
-        .route("/generate-password", 
-        get(generate_password))
-        .route("/api/admin/vaults", 
-        get(get_all_async).post(create_async))
-        .route("/api/admin/vaults/{id}",
-        get(get_by_id_async).put(update_async).delete(delete_async)
+        .route("/", get(|| async { "Hello World" })) // inline async handler
+        .route("/generate-password", get(generate_password))
+        .route("/api/admin/vaults", get(get_all_async).post(create_async))
+        .route(
+            "/api/admin/vaults/:id",
+            get(get_by_id_async).put(update_async).delete(delete_async),
         )
 }
